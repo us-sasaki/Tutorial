52891c0e0b9b1ca0de9075de1b1cf3b68c960024
us-sasaki <syusuke@pony.ocn.ne.jp>
2020-02-23 20:33:01 +0900

    1

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 // http モジュール読込
 const http = require('http');
 
-let lastRequested = {};
+let lastRequested = [];
 
 // サーバ構築
 const server = http.createServer(function (request, response) {
@@ -20,7 +20,11 @@ const server = http.createServer(function (request, response) {
     if (request.method === 'GET') {
         // GET 時の処理
         response.write('last requested:\r\n');
-        response.write(JSON.stringify(lastRequested));
+        for (let i = 0; i < lastRequested.length; i++) {
+            response.write('No.' + i);
+            response.write(lastRequested[i]);
+            response.write('\r\n');
+        }
         response.end(); // レスポンス送信を完了する
     } else if (request.method === 'POST') {
         // POST 時の処理
@@ -28,6 +32,7 @@ const server = http.createServer(function (request, response) {
         request.on('data', (chunk) => {
             postData += chunk;
         }).on('end', () => {
+            lastRequested.push(postData);
             response.end('あなたが送信したのは、'+postData);
         })
     }
