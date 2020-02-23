a8e750d91b5f0d24afef51194531121b9790600c
us-sasaki <syusuke@pony.ocn.ne.jp>
2020-02-23 20:15:46 +0900

    draft web-server

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-var http = require('http'); //httpモジュール呼び出し
-var server = http.createServer(function (request, response) {
-    // リクエストを受けると以下のレスポンスを送信する
-    response.writeHead(200, {'Content-Type': 'text/plain'}); //レスポンスヘッダーに書き込み
-    response.write('Hello World\n'); // レスポンスボディに「Hello World」を書き込み
-    response.end(); // レスポンス送信を完了する
+'use strict';
+
+/**
+ * Heroku 用簡易 Web サーバ(http 利用)
+ * 
+ * @author  Yusuke Sasaki
+ */
+// http モジュール読込
+const http = require('http');
+
+let lastRequested = {};
+
+// サーバ構築
+const server = http.createServer(function (request, response) {
+    // リクエストを受けた時の処理
+    // レスポンスヘッダ
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    //
+    if (request.method === 'GET') {
+        // GET 時の処理
+        response.write('last requested:\r\n');
+        response.write(JSON.stringify(lastRequested));
+        response.end(); // レスポンス送信を完了する
+    } else if (request.method === 'POST') {
+        // POST 時の処理
+        let postData = '';
+        request.on('data', (chunk) => {
+            postData += chunk;
+        }).on('end', () => {
+            response.end('あなたが送信したのは、'+postData);
+        })
+    }
 });
+
+// サーバ起動
 server.listen(process.env.PORT || 8080);  //8080番ポートで待ち受け
