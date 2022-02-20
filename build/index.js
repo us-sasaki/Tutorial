6d4f8a67642774a37ac04ffce42cca2b520d4d44
us-sasaki <syusuke@pony.ocn.ne.jp>
2022-02-21 08:31:45 +0900

    umd2

--- build/index.js
+++ build/index.js
@@ -1,14 +1,19 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['exports', 'b'], factory);
+    } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
+        // CommonJS
+        factory(exports, require('b'));
+    } else {
+        // Browser globals
+        factory((root.commonJsStrict = {}), root.b);
     }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
+}(this, function (exports, b) {
+    //use b in some fashion.
+
+    // attach properties to the exports object to define
+    // the exported module properties.
     exports.Person = void 0;
     class Person {
         constructor(name) {
@@ -22,5 +27,5 @@
         }
     }
     exports.Person = Person;
-});
+}));
 //# sourceMappingURL=index.js.map
